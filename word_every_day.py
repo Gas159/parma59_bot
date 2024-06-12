8c0a7228a748cd9670e397247a6bb1a73c8057dd
gas <pankot222@gmail.com>
2024-06-12 12:54:40 +0500

    upd word

--- word_every_day.py
+++ word_every_day.py
@@ -2,8 +2,10 @@ import asyncio
 import logging
 import random
 import sys
+from datetime import datetime
 from typing import Callable, Dict, Any, Awaitable
 
+from aiogram.methods import send_message
 from apscheduler.schedulers.asyncio import AsyncIOScheduler
 import requests
 from aiogram import Bot, Dispatcher, BaseMiddleware
@@ -64,19 +66,19 @@ text = [i.strip('') for i in lines if i.strip()]
 
 
 # Add middleware Scheduler
-class SchedulerMiddleware(BaseMiddleware):
-    def __init__(self, scheduler: AsyncIOScheduler):
-        self.scheduler = scheduler
-
-    async def __call__(
-            self,
-            handler: Callable[[TelegramObject, Dict[str, Any]], Awaitable[Any]],
-            event: TelegramObject,
-            data: Dict[str, Any],
-    ) -> Any:
-        # add apscheduler to data
-        data["apscheduler"] = self.scheduler
-        return await handler(event, data)
+# class SchedulerMiddleware(BaseMiddleware):
+#     def __init__(self, scheduler: AsyncIOScheduler):
+#         self.scheduler = scheduler
+#
+#     async def __call__(
+#             self,
+#             handler: Callable[[TelegramObject, Dict[str, Any]], Awaitable[Any]],
+#             event: TelegramObject,
+#             data: Dict[str, Any],
+#     ) -> Any:
+#         # add apscheduler to data
+#         data["apscheduler"] = self.scheduler
+#         return await handler(event, data)
 
 
 @dp.message(F.text.lower().startswith(("й", 'q', 'cat', "кот")))
@@ -102,32 +104,24 @@ async def send_text_test(message: Message):
     try:
         print(message.model_dump_json(indent=4, exclude_none=True))
         phrase = next(shuffle(text))
+        print(type(phrase),phrase)
         # await message.answer(text=f"Сообщение отправлено.")отправлено
         await message.answer(text=phrase)
     except Exception as e:
         logging.exception(e)
 
 
-async def scheduler():
-    # aioschedule.every().day.at("17:45").do(send_text_test1)
-    aioschedule.every(3).seconds.do(send_text_test1)
-    while True:
-        await aioschedule.run_pending()
-        await asyncio.sleep(1)
 
-
-async def send_text_test1(message: Message = None):
+async def send_text_test1():
     try:
         # print(message.model_dump_json(indent=4, exclude_none=True))
         phrase = next(shuffle(text))
         # await message.answer(text=f"Сообщение отправлено.")отправлено
-        await message.answer(text=phrase)
+        # await message.answer(text=phrase)
+        print(phrase)
+        return phrase
     except Exception as e:
         logging.exception(e)
-
-
-async def on_startup():
-    asyncio.create_task(scheduler())
 
 
 async def delete_webhook_and_handle_updates():
@@ -135,29 +129,62 @@ async def delete_webhook_and_handle_updates():
     await bot.delete_webhook(drop_pending_updates=True)
 
     # Начало обработки обновлений через периодический опрос
-    await dp.start_polling(bot, on_startup=on_startup)
+    await dp.start_polling(bot)
+
+
+# async def main() -> None:
+#     # Initialize Bot instance with a default parse mode which will be passed to all API calls
+#     bot = Bot(BOT_TOKEN)
+#     # And the run events dispatching
+#     scheduler = AsyncIOScheduler()
+#     timezone = "Europe/Moscow"
+#
+#     dp.update.middleware(
+#         SchedulerMiddleware(scheduler=scheduler),
+#     )
+#     scheduler.start()
+#     # await on_startup()
+#     await dp.start_polling(bot)
+
+
+# # Запуск асинхронной функции
+# if __name__ == '__main__':
+#     loop = asyncio.get_event_loop()
+#     loop.run_until_complete(delete_webhook_and_handle_updates())
+
+async def send_message(bot: Bot,user_id: int):
+    """Функция для отправки сообщения пользователю."""
+    try:
+        phrase = next(shuffle(text))
+        print(type(phrase), phrase)
+        await bot.send_message(chat_id=user_id, text=phrase)
+        logging.info("Message sent successfully")
+    except Exception as e:
+        logging.error(f"Error sending message: {e}")
 
 
 async def main() -> None:
+    # Удаление вебхука
+    await bot.delete_webhook(drop_pending_updates=True)
+
     # Initialize Bot instance with a default parse mode which will be passed to all API calls
-    bot = Bot(BOT_TOKEN)
+    # bot = Bot(BOT_TOKEN)  # (, parse_mode=ParseMode.HTML)
     # And the run events dispatching
     scheduler = AsyncIOScheduler()
     timezone = "Europe/Moscow"
 
-    dp.update.middleware(
-        SchedulerMiddleware(scheduler=scheduler),
-    )
+    scheduler.add_job(send_message, trigger="interval", seconds=3,  start_date=datetime.now(), kwargs={
+        "bot": bot,
+        # "message": next(shuffle(text)),
+        # 'message': 'opo',
+        # "user_id": ,
+        "user_id": -4161841389
+    }, )
     scheduler.start()
-    # await on_startup()
+
     await dp.start_polling(bot)
 
 
 if __name__ == "__main__":
     logging.basicConfig(level=logging.INFO, stream=sys.stdout)
     asyncio.run(main())
-
-# # Запуск асинхронной функции
-# if __name__ == '__main__':
-#     loop = asyncio.get_event_loop()
-#     loop.run_until_complete(delete_webhook_and_handle_updates())
