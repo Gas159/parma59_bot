b24bb6db33c9278ec3f2669ad63b1e8c3c25a73f
gas <pankot222@gmail.com>
2024-06-28 03:13:54 +0500

    upd word

--- lessons/psycopg2_.py
+++ lessons/psycopg2_.py
@@ -2,22 +2,28 @@ import psycopg2
 
 # Подключение к базе данных
 conn = psycopg2.connect(
-    dbname="testdb",
-    user="testuser",
-    password="123",
-    host="localhost"
+	dbname="testdb",
+	user="testuser",
+	password="123",
+	host="localhost"
 )
-
+print(conn)
 # Создание курсора
 cur = conn.cursor()
 
 # Создание новой записи
-cur.execute("INSERT INTO mytable (column1, column2) VALUES (%s, %s)", ("value1", "value2"))
+cur.execute("INSERT INTO users (name, age) VALUES (%s, %s)", ("value1", 32))
 
 # Фиксация изменений
 conn.commit()
 
+#view data
+cur.execute("SELECT * FROM users")
+rows = cur.fetchall()
+for row in rows:
+    print(row)
+
+
 # Закрытие курсора и соединения
 cur.close()
 conn.close()
-
